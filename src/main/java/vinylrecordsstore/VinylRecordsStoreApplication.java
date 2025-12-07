5aef870450c671174190ae9191caebcf58de3b16
mikhail <1028880@edu.rut-miit.ru>
2025-12-08 01:11:54 +0300

    Initial commit

---
+++ src/main/java/vinylrecordsstore/VinylRecordsStoreApplication.java
@@ -0,0 +1,13 @@
+package vinylrecordsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylRecordsStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinylRecordsStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vinylrecordsstore/VinylRecordsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vinylrecordsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylRecordsStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
